47e4e8855cf872c0689b9e88d1031773f1e93edb
TzuyusForgottenLuggage <kkd999.km@gmail.com>
2022-05-09 12:07:46 +0530

    Updated Progress

--- public_html/d3bar.js
+++ public_html/d3bar.js
@@ -21,11 +21,11 @@ const data = [
   const x = d3.scaleBand()
     .domain(d3.range(data.length))
     .range([margin.left, width - margin.right])
-    .padding(0.1)
+    .padding(0.1);
   
   const y = d3.scaleLinear()
     .domain([0, 100])
-    .range([height - margin.bottom, margin.top])
+    .range([height - margin.bottom, margin.top]);
   
   svg
     .append("g")
@@ -43,13 +43,13 @@ const data = [
     g.attr("transform", `translate(${margin.left}, 0)`)
       .call(d3.axisLeft(y).ticks(null, data.format))
       .attr("font-size", '20px')
-  }
+  };
   
   function xAxis(g) {
     g.attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x).tickFormat(i => data[i].name))
       .attr("font-size", '20px')
-  }
+  };
   
   svg.append("g").call(xAxis);
   svg.append("g").call(yAxis);
